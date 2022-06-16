aeb02e196438356333d259132b43a7fae51c4607
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-06-17 00:30:05 +0300

    commit tree 6e6e0c085738588ceec9fa60514c7ecd52c0ba07

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
+function reverseString(word){
+
+return word.split("").reverse("").join("")
+
+}
+
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  // I need to make a palidrine function that will allow the user to know if the 
+  //..if the word input reversed is the same as the original word e.g "mom" :true, "leg" : false
+const reversedWord = reverseString(word)
+
+//confirm if the reversed string is the same as the input.
+
+return reversedWord === word
+  
 }
 
 /* 
   Add your pseudocode here
+reverse the input string
+
+if the reversed string is the same as the input
+  return true
+else
+  return false
+
 */
 
 /*
@@ -20,6 +41,27 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"))
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("dog"))
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("dad"))
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("madam"))
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("king"))
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("wow"))
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("z"))
 }
 
 module.exports = isPalindrome;
